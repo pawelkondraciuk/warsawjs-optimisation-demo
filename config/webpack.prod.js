1796b51ddb78556b9157cd0244a07b0f9e81e5b1
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2017-06-08 01:22:12 +0200

    Add minification

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -18,6 +18,28 @@ module.exports = webpackMerge(commonConfig, {
 
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      beautify: false,
+      output: {
+        comments: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      compress: {
+        screw_ie8: true,
+        warnings: false,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+    }),
     new ExtractTextPlugin('[name].css'),
     new webpack.DefinePlugin({
       'process.env': {
