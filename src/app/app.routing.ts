117fdeea9f3063741922e2e62064165872f5a6c1
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2017-06-08 21:47:57 +0200

    Add preloading strategy

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AboutComponent } from './about';
 import { HomeComponent } from './home';
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
