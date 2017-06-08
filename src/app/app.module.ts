b1dc2321000d6621add4c9e6414c7412259a98e2
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2017-06-08 02:55:01 +0200

    Add module tree shaking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
@@ -16,8 +15,7 @@ import { RouteTwoModule } from './+route-two';
 @NgModule({
   imports: [
     BrowserModule,
-    HttpModule,
-  
+
     AppRoutingModule,
 
     RouteOneModule,
